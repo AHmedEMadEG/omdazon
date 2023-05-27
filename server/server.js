54f3d056a40a32ee6543571ed805dcb7efd3b4cf
AHmedEMadEG <amoemo141996@gmail.com>
2023-05-27 17:14:46 +0300

    deploying updates

--- server/server.js
+++ server/server.js
@@ -32,10 +32,10 @@ require('./app/routes/payment.routes')(app);
 
 
 // for render deploying
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
